dc0625bc552af91e34b1870730863fb6a03f80f7
Ridhwan Adagiri <ibrahimridwan47@gmail.com>
2019-11-23 07:42:03 -1100

    recent updates

--- src/app.js
+++ src/app.js
@@ -29,20 +29,20 @@ const renderApp = () => {
 
 ReactDOM.render(<LoadingPage />, document.getElementById("app"));
 
-// firebase.auth().onAuthStateChanged(user => {
-//   if (user) {
-//     console.log("logged in");
-//     store.dispatch(login(user.uid));
-//     store.dispatch(startSetExpenses()).then(() => {
-//       renderApp();
-//       if (history.location.pathname === "/") {
-//         history.push("/dashboard");
-//       }
-//     });
-//   } else {
-//     console.log("logged out");
-//     store.dispatch(logout());
-//     renderApp();
-//     history.push("/");
-//   }
-// });
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log("logged in");
+    store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === "/") {
+        history.push("/dashboard");
+      }
+    });
+  } else {
+    console.log("logged out");
+    store.dispatch(logout());
+    renderApp();
+    history.push("/");
+  }
+});
