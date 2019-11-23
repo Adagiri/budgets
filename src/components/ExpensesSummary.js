49374a3bb511d9f9dd25275f3de86a9264a6cd73
Ridhwan Adagiri <ibrahimridwan47@gmail.com>
2019-11-22 18:25:14 -1100

    added more styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,18 +1,24 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 import ExpenseTotal from "../selectors/ExpensesTotal";
 import { connect } from "react-redux";
 import getVisibleExpenses from "../selectors/getVisibleExpenses";
 import numeral from "numeral";
 
 const ExpensesSummary = props => (
-  <div>
-  <h1>
-    Viewing {props.expenses.length} expenses totalling{" "}
-    {numeral(ExpenseTotal(props.expenses) / 100).format("$0,0.00")}
+  <div className="page-header">
+  <div className="content-container">
+  <h1 className="page-header__title">
+    Viewing <span>{props.expenses.length}</span> expenses totalling{" "}
+    <span>{numeral(ExpenseTotal(props.expenses) / 100).format("$0,0.00")}</span>
   </h1>
     {
-      props.unfiltered.length > props.expenses.length ? <h2> Not viewing {props.unfiltered.length - props.expenses.length} expense because of filters</h2> : ""
+      props.unfiltered.length > props.expenses.length ? <h3 className="page-header__title not-viewing"> Not viewing <span>{props.unfiltered.length - props.expenses.length}</span> expense because of filters</h3> : ""
     }
+    <div className="page-header__action">
+    <Link className="button" to="/create">Add Expense</Link>
+    </div>
+  </div>
   </div>
 );
 
